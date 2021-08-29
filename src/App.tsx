368c7a759d9f2a5183f2fa62f61b0ea2231a46eb
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-08-29 15:13:42 -0300

    impl: basic structure modal ann modal style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import Modal from 'react-modal';
+import { useState } from 'react';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
+import { NewTransationModal } from './components/NewTransactionModal';
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleClosenewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <NewTransationModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleClosenewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
